d2273819191f621f4244c336fe6c6bf1b3e6480c
Seth McNeill <seth.mcneill@ucollege.edu>
2021-01-29 10:45:45 -0600

    Modified main.cpp date

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * @brief This is a demonstration for class of using a GitHub Template
  * @details This project shows Doxygen, CMake, and GitHub Pages working together
  * @author Seth McNeill
- * @date 1/29/2021
+ * @date 2021 January 29
  * 
  */
 
